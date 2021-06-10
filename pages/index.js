b31843315f1d6f364ed0e1a796711fbd87092747
Peter Meehan <peter.meehan@intercom.io>
2021-06-10 16:05:31 +0100

    Add a dynamic api endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  const generateNewRandomNumber = _clickEvent => {
+    fetch("/api/random-gen")
+      .then(res => res.json())
+      .then(
+        result => {
+          setRandomNumber(result);
+        },
+        error => {
+          console.error("uhh ohh", error);
+        }
+      );
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +32,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Feature 4: Pink site</h1>
+        <div>Today's random number is: {randomNumber}</div>
+        <button onClick={generateNewRandomNumber}>Change random number</button>
+
         <div className={styles.footer}>
           <span style={{ marginRight: "0.5rem" }}>How does</span>
           <Image
